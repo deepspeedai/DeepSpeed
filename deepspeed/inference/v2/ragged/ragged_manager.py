deepspeedai/DeepSpeed

commit bd555449731b680834331ad1803c82f6df36fecf

KV Cache Improved Flexibility (#4668)
This KV-cache adds the foundation for appropriately supporting two key
KV-cache improvements:

1. Delineation between local/dense KV caches/models at the cache level
in addition to the attention module level.
2. Support for multiple types of disjoint KV caches (such as alternating
local + dense attention GPT-Neo).

Follow up item: Determine appropriate statistics for weighting local +
dense KV block ratios when both are present.

---------

Co-authored-by: Olatunji Ruwase <olruwase@microsoft.com>

deepspeed/inference/v2/ragged/ragged_manager.py
@@ -28,7 +28,7 @@ class DSStateManager:
     should come from the engine config.
     """
 
-    _kv_config: KVCacheConfig
+    _kv_configs: Tuple[KVCacheConfig]
     """
     Config for the KV cache. See KVCacheConfig for more details. These arguments should derive
     from the model implementation.
@@ -49,13 +49,12 @@ class DSStateManager:
 
     # Allocator for tracking sequences.
     _tracking_allocator: BlockedAllocator
-    _all_block_ids: torch.Tensor
-    _all_block_ids_shadow: torch.Tensor
+    _all_block_ids: Tuple[torch.Tensor, ...]
+    _all_block_ids_shadow: Tuple[torch.Tensor, ...]
 
-    # TODO(cmikeh2): This interface both needs to be more flexible and more concrete.
     def __init__(self,
                  config: DSStateManagerConfig,
-                 kv_config: KVCacheConfig,
+                 kv_configs: Tuple[KVCacheConfig, ...],
                  base_mp_group: Optional[Any] = None) -> None:
         """
         The key
@@ -64,7 +63,7 @@ class DSStateManager:
             block_size (int): The number of tokens to allocate in each block.
         """
         self._config = config
-        self._kv_config = kv_config
+        self._kv_configs = kv_configs
 
         # Load our helpers for host allocation.
         self._ragged_utils = RaggedUtilsBuilder().load()
@@ -72,48 +71,41 @@ class DSStateManager:
         # Initialize the allocator for tracking sequences (so this doesn't need to be ad-hoc).
         self._tracking_allocator = BlockedAllocator(self._config.max_tracked_sequences)
 
-        # Storage to back tracking the KV cache allocation.
-        ids_shape = (
-            self._config.max_tracked_sequences,
-            self._kv_config.num_allocation_groups,
-            self._kv_config.max_blocks_per_allocation_group,
-        )
-        self._all_block_ids = torch.zeros(ids_shape, dtype=torch.int32, device=get_accelerator().current_device())
-        self._all_block_ids_shadow = self._ragged_utils.allocate_fast_host_buffer(self._all_block_ids)
+        all_block_ids = []
+        all_block_ids_shadow = []
+
+        for cache_config in self._kv_configs:
+            # Storage to back tracking the KV cache allocation.
+            ids_shape = (
+                self._config.max_tracked_sequences,
+                cache_config.num_allocation_groups,
+                cache_config.max_blocks_per_allocation_group,
+            )
+
+            all_block_ids.append(torch.zeros(ids_shape, dtype=torch.int32, device=get_accelerator().current_device()))
+            all_block_ids_shadow.append(self._ragged_utils.allocate_fast_host_buffer(all_block_ids[-1]))
+
+        self._all_block_ids = tuple(all_block_ids)
+        self._all_block_ids_shadow = tuple(all_block_ids_shadow)
 
         # Initialize the sequence container.
         self._seqs = {}
 
         # Finally initialize the KV cache.
-        self._kv_cache = BlockedKVCache(self._kv_config,
+        self._kv_cache = BlockedKVCache(self._kv_configs,
                                         self._config.memory_config,
                                         mp_group=base_mp_group,
                                         offload=self._config.offload)
 
-    def get_cache(self, cache_id: int) -> torch.Tensor:
+    def get_cache(self, cache_id: int, cache_group: int = 0) -> torch.Tensor:
         """
-        Return the Tensor associated with the given cache id.
-        """
-        return self._kv_cache.get_cache(cache_id)
+        Return the Tensor associated with the given cache id in the specified cache group.
 
-    def query(self, uid: Optional[int] = None) -> Tuple[int, int, Optional[int]]:
+        Arguments:
+            cache_group (str): The KV cache group.
+            cache_id (int): The cache id within that group.
         """
-        Query the state of the KV cache for occupancy.
-
-        Parameters:
-            seq_id (Optional[int]): The sequence id to query. If None, the last
-                return value will be None.
-
-        Returns:
-            Tuple[int, int, Optional[Tuple[int, int]]: A tuple of the block size, the number of
-                free blocks, and the number of cached tokens for the given sequence.
-        """
-        if uid is not None:
-            cached_toks = self._seqs[uid].cached_tokens
-            free_toks = cached_toks % self._block_size
-            return (self._block_size, self._kv_cache.free_blocks, free_toks)
-        else:
-            return (self._block_size, self._kv_cache.free_blocks, None)
+        return self._kv_cache.get_cache(cache_id, cache_group=cache_group)
 
     def flush_sequence(self, uid: int) -> None:
         """
@@ -124,7 +116,9 @@ class DSStateManager:
             return
 
         seq = self._seqs[uid]
-        self._kv_cache.free(seq.all_block_ids)
+        for i in range(self.n_kv_caches):
+            self._kv_cache.free(seq.all_block_ids(cache_group=i), cache_group=i)
+
         self._tracking_allocator.free(seq.tracking_id)
         del self._seqs[uid]
 
@@ -163,12 +157,15 @@ class DSStateManager:
             raise RuntimeError(
                 f"Unable to create tracking slot for sequence {uid} since the metadata buffers are full.")
 
-        seq_block_ids = self._all_block_ids[tracking_slot]
-        seq_block_ids_shadow = self._all_block_ids_shadow[tracking_slot]
+        seq_block_ids = tuple(all_block_ids[tracking_slot] for all_block_ids in self._all_block_ids)
+        seq_block_ids_shadow = tuple(all_block_ids_shadow[tracking_slot]
+                                     for all_block_ids_shadow in self._all_block_ids_shadow)
+
         self._seqs[uid] = DSSequenceDescriptor(tracking_slot,
                                                seq_block_ids,
                                                seq_block_ids_shadow,
                                                max_context=self._config.max_context)
+        # TODO(cmikeh2): Debug call here might be unnecessary and is potentially on critical path.
         logger.debug(f"Created sequence {uid} with tracking slot {tracking_slot}.")
         return self._seqs[uid]
 
@@ -194,11 +191,18 @@ class DSStateManager:
         return self._kv_config.block_size
 
     @property
-    def free_blocks(self) -> int:
+    def n_kv_cache_groups(self) -> int:
+        """
+        Return the number of KV caches.
+        """
+        return self._kv_cache.num_caches
+
+    @property
+    def free_blocks(self) -> torch.Tensor:
         """
         Return the number of free blocks in the KV cache.
         """
         return self._kv_cache.free_blocks
 
-    def allocate_blocks(self, n_blocks: int) -> torch.Tensor:
-        return self._kv_cache.reserve(n_blocks)
+    def allocate_blocks(self, n_blocks: int, cache_group: int = 0) -> torch.Tensor:
+        return self._kv_cache.reserve(n_blocks, cache_group=cache_group)

deepspeed/inference/v2/ragged/sequence_descriptor.py
@@ -3,7 +3,7 @@
 
 # DeepSpeed Team
 
-from typing import List, Union
+from typing import List, Tuple, Union
 
 import torch
 
@@ -18,14 +18,14 @@ class BaseSequenceDescriptor:
         raise NotImplementedError()
 
     @property
-    def cur_allocated_blocks(self) -> int:
+    def cur_allocated_blocks(self, cache_group: int = 0) -> int:
         """
         The number of KV blocks currently allocated for this sequence.
         """
         raise NotImplementedError()
 
     @property
-    def kv_blocks_ptr(self) -> int:
+    def kv_blocks_ptr(self, cache_group: int = 0) -> int:
         """
         The pointer to the KV blocks for this sequence.
         """
@@ -48,11 +48,11 @@ class PlaceholderSequenceDescriptor(BaseSequenceDescriptor):
         return self._seen_tokens
 
     @property
-    def cur_allocated_blocks(self) -> int:
+    def cur_allocated_blocks(self, cache_group: int = 0) -> int:
         return self._cur_allocated_blocks
 
     @property
-    def kv_blocks_ptr(self) -> int:
+    def kv_blocks_ptr(self, cache_group: int = 0) -> int:
         return self._kv_blocks_ptr
 
 
@@ -74,19 +74,19 @@ class DSSequenceDescriptor(BaseSequenceDescriptor):
     may be used in future implementations for speculative caching.
     """
 
-    _num_allocation_groups: int
+    _num_allocation_groups: Tuple[int, ...]
     """
-    Number of unique allocation groups associated with the sequence.
+    Number of unique allocation groups associated with the sequence for each cache group.
     """
 
-    _blocks_per_allocation_group: torch.IntTensor
+    _blocks_per_allocation_group: Tuple[torch.IntTensor, ...]
     """
-    Number of blocks allocated for each allocation group.
+    Number of blocks allocated for each allocation group in each cache group.
     """
 
     # Padded list of KV-cache IDs for the sequence.
-    _kv_cache_ids: torch.Tensor
-    _kv_cache_ids_shadow: torch.Tensor
+    _kv_cache_ids: Tuple[torch.Tensor, ...]
+    _kv_cache_ids_shadow: Tuple[torch.Tensor, ...]
     """
     Padded list of KV-cache IDs for the sequence. The padded shape is [num_allocation_groups, max_blocks_per_allocation_group].
     """
@@ -97,81 +97,142 @@ class DSSequenceDescriptor(BaseSequenceDescriptor):
 
     def __init__(self,
                  tracking_id: int,
-                 kv_cache_ids: torch.Tensor,
-                 kv_cache_ids_shadow: torch.Tensor,
+                 kv_cache_ids: Tuple[torch.Tensor, ...],
+                 kv_cache_ids_shadow: Tuple[torch.Tensor, ...],
                  max_context: int = -1) -> None:
+        """
+        Create the metadata to track a single sequence in the system.
+
+        Arguments:
+            tracking_id (int): The slot in the tracking buffers used to track this sequence.
+            kv_cache_ids (Tuple[torch.Tensor, ...]): The KV-cache IDs for the sequence. The shape
+                of the tensor should be [num_allocation_groups, max_blocks_per_allocation_group].
+                There should be one tensor per cache group.
+            kv_cache_ids_shadow (Tuple[torch.Tensor, ...]): The shadow tensor for the KV-cache IDs.
+                This tensor should be allocated on the host and should have the same shape as the
+                tensor provided in ``kv_cache_ids``. There should be one tensor per cache group.
+            max_context (int): The maximum number of tokens this sequence may eventually include.
+                Currently unused but may be used in future implementations for speculative caching.
+        """
         self._tracking_id = tracking_id
         self._kv_cache_ids = kv_cache_ids
         self._kv_cache_ids_shadow = kv_cache_ids_shadow
         self._max_context = max_context
+        self._n_cache_groups = len(kv_cache_ids)
 
         self._seen_tokens = 0
         self._in_flight_tokens = 0
 
-        self._num_allocation_groups = kv_cache_ids_shadow.shape[0]
-        self._blocks_per_allocation_group = torch.zeros(self._num_allocation_groups, dtype=torch.int32, device="cpu")
+        self._num_allocation_groups = tuple(kv_cache_ids_shadow.shape[0]
+                                            for kv_cache_ids_shadow in kv_cache_ids_shadow)
+        self._blocks_per_allocation_group = tuple(
+            torch.zeros(num_groups, dtype=torch.int32, device="cpu") for num_groups in self._num_allocation_groups)
 
-        assert kv_cache_ids.shape[0] == self._num_allocation_groups
-        assert len(kv_cache_ids.shape) == 2
+        for cache_group, kv_cache_ids in enumerate(kv_cache_ids):
+            assert self._num_allocation_groups[cache_group] == kv_cache_ids.shape[0]
+            assert len(kv_cache_ids.shape) == 2
 
     @property
     def seen_tokens(self) -> int:
+        """
+        Number of tokens in the sequence that have completed a forward pass.
+        """
         return self._seen_tokens
 
     @property
     def in_flight_tokens(self) -> int:
+        """
+        Number of tokens that have begun a forward pass but not yet completed it.
+        """
         return self._in_flight_tokens
 
     @property
     def max_context(self) -> int:
+        """
+        Maximum number of tokens for this sequence. Currently unused.
+        """
         return self._max_context
 
     @property
-    def cur_allocated_blocks(self) -> int:
-        return self._blocks_per_allocation_group.sum()
-
-    @property
     def tracking_id(self) -> int:
+        """
+        Return the slot in the tracking buffers used to track this sequence.
+        """
         return self._tracking_id
 
-    def kv_cache_ids(self, on_device: bool = False) -> torch.Tensor:
+    @property
+    def cur_allocated_blocks(self, cache_group: int = 0) -> int:
         """
-        Returns the Tensor containing the block IDs for this sequence on the appropriate device.
+        Returns the number of blocks currently allocated for this sequence in the specified cache group.
+
+        Arguments:
+            cache_group (int): The cache group to query.
+        """
+        return self._blocks_per_allocation_group[cache_group].sum()
+
+    def kv_cache_ids(self, cache_group: int = 0, on_device: bool = False) -> torch.Tensor:
+        """
+        Returns the Tensor containing the block IDs for this sequence on the appropriate device
+        for the specified cache group.
+
+        Arguments:
+            cache_group (int): The cache group to query.
+            on_device (bool): Whether or not to return the Tensor on the device or on the host.
         """
         if on_device:
-            return self._kv_cache_ids
+            return self._kv_cache_ids[cache_group]
         else:
-            return self._kv_cache_ids_shadow
+            return self._kv_cache_ids_shadow[cache_group]
 
     @property
-    def kv_blocks_ptr(self) -> int:
-        return self._kv_cache_ids.data_ptr()
+    def kv_blocks_ptr(self, cache_group: int = 0) -> int:
+        """
+        Get the device pointer to the base of the KV-cache ids for the specified cache group and
+        sequence.
+
+        Arguments:
+            cache_group (int): The cache group to query.
+        """
+        return self._kv_cache_ids[cache_group].data_ptr()
 
     @property
-    def all_block_ids(self) -> torch.Tensor:
+    def all_block_ids(self, cache_group: int = 0) -> torch.Tensor:
+        """
+        Return the Tensor containing all block IDs for this sequence in the specified cache group.
+
+        Arguments:
+            cache_group (int): The cache group to query.
+        """
         block_ids = []
-        for allocation_group, num_blocks in zip(self._kv_cache_ids, self._blocks_per_allocation_group):
+        for allocation_group, num_blocks in zip(self._kv_cache_ids[cache_group],
+                                                self._blocks_per_allocation_group[cache_group]):
             block_ids.append(allocation_group[:num_blocks])
         return torch.cat(block_ids)
 
     def pre_forward(self, num_tokens: int) -> None:
         """
         Update the state of the sequence before a forward pass.
+
+        Arguments:
+            num_tokens (int): The number of tokens in the sequence that will be executed during the
+                next forward pass of the model.
         """
         self._in_flight_tokens = num_tokens
 
     def post_forward(self) -> None:
         """
-        Update the state of the sequence after a forward pass.
+        Update the state of the sequence after a forward pass. This should be called after the forward
+        pass completes. NOTE: due to the asynchronous nature of the accelerator, this may be called
+        before the forward pass completes on the device itself.
         """
         self._seen_tokens += self._in_flight_tokens
         self._in_flight_tokens = 0
 
-    def extend_kv_cache(self, new_ids: Union[List[torch.IntTensor], torch.IntTensor]) -> None:
+    def extend_kv_cache(self, new_ids: Union[List[torch.IntTensor], torch.IntTensor], cache_group: int = 0) -> None:
         """
         Extend the KV-cache for the sequence.
 
-        Args:
+        Arguments:
             new_ids (Union[List[torch.IntTensor], torch.IntTensor]): For each allocation group, the IDs
                 to add to the KV-cache. If there is only one allocation group, a single tensor can be
                 provided. Otherwise, a list of tensors should be provided. The tensors do not need
@@ -180,8 +241,9 @@ class DSSequenceDescriptor(BaseSequenceDescriptor):
         if isinstance(new_ids, torch.Tensor):
             new_ids = [new_ids]
 
-        if len(new_ids) != self._num_allocation_groups:
-            raise ValueError(f"Only {len(new_ids)} allocation groups provided, expected {self._num_allocation_groups}")
+        if len(new_ids) != self._num_allocation_groups[cache_group]:
+            raise ValueError(
+                f"Only {len(new_ids)} allocation groups provided, expected {self._num_allocation_groups[cache_group]}")
 
         for group_id, new_group_ids in enumerate(new_ids):
             new_blocks = new_group_ids.numel()
@@ -190,22 +252,22 @@ class DSSequenceDescriptor(BaseSequenceDescriptor):
                 # If we have multiple groups, it's possible to have an empty group.
                 continue
 
-            shadow_alloc_group = self._kv_cache_ids_shadow[group_id]
-            alloc_group = self._kv_cache_ids[group_id]
-            cur_blocks = self._blocks_per_allocation_group[group_id]
+            shadow_alloc_group = self._kv_cache_ids_shadow[cache_group][group_id]
+            alloc_group = self._kv_cache_ids[cache_group][group_id]
+            cur_blocks = self._blocks_per_allocation_group[cache_group][group_id]
 
             shadow_alloc_group[cur_blocks:cur_blocks + new_blocks].copy_(new_group_ids)
             alloc_group[cur_blocks:cur_blocks + new_blocks].copy_(shadow_alloc_group[cur_blocks:cur_blocks +
                                                                                      new_blocks],
                                                                   non_blocking=True)
 
-            self._blocks_per_allocation_group[group_id] += new_blocks
+            self._blocks_per_allocation_group[cache_group][group_id] += new_blocks
 
-    def free_kv_cache(self, free_ids: Union[List[torch.IntTensor], torch.IntTensor]) -> None:
+    def free_kv_cache(self, free_ids: Union[List[torch.IntTensor], torch.IntTensor], cache_group: int = 0) -> None:
         """
         Free blocks from the KV-cache for the sequence.
 
-        Args:
+        Arguments:
             free_ids (Union[List[torch.IntTensor], torch.IntTensor]): The ids of blocks to free
                 from the KV-cache. If there is only one allocation group, a single tensor can be
                 provided. Otherwise, a list of tensors should be provided. The tensors do not need

deepspeed/inference/v2/engine_v2.py
@@ -45,13 +45,20 @@ class InferenceEngineV2:
     """
 
     @property
-    def free_blocks(self) -> int:
+    def free_blocks(self) -> torch.Tensor:
         """
-        Number of free KV blocks.
+        Number of free KV blocks. This is a tensor of shape [n_kv_cache_groups] where each
+        element is the number of free blocks in the corresponding KV cache group.
         """
         return self._state_manager.free_blocks
 
     @property
+    def n_kv_cache_groups(self) -> int:
+        """
+        Number of KV cache groups.
+        """
+        return self._state_manager.n_kv_cache_groups
+
     def model(self) -> DSInferenceModelBase:
         """
         The model implementation.
@@ -143,7 +150,7 @@ class InferenceEngineV2:
 
         return logits
 
-    def query(self, uid: int, max_request_tokens: int, max_request_blocks) -> Tuple[int, int]:
+    def query(self, uid: int, max_request_tokens: int, max_request_blocks) -> Tuple[int, torch.Tensor]:
         """
         Determine the number of tokens and KV blocks to reserve for a given request. Given a UID
         (this UID may not be recognized by the model yet), this will return the number of tokens

deepspeed/inference/v2/model_implementations/common_architectures/__init__.py
@@ -1,4 +1,0 @@
-# Copyright (c) Microsoft Corporation.
-# SPDX-License-Identifier: Apache-2.0
-
-# DeepSpeed Team

deepspeed/inference/v2/model_implementations/inference_model_base.py
@@ -180,7 +180,7 @@ class DSInferenceModelBase(torch.nn.Module, ABC):
 
     @abstractmethod
     def get_kv_requirements(self, sequence: DSSequenceDescriptor, max_new_tokens: int,
-                            max_new_blocks: int) -> Tuple[int, int]:
+                            max_new_blocks: Tuple[int, ...]) -> Tuple[int, torch.Tensor]:
         """
         Given a sequence and the number of new tokens in the sequence, determine the
         number of new KV blocks needed to support the sequence. This method is
@@ -193,9 +193,9 @@ class DSInferenceModelBase(torch.nn.Module, ABC):
             max_new_blocks (int): Maximum number of blocks to hypothetically allocate.
 
         Returns:
-            Tuple[int, int]: The tuple of number of tokens scheduled and number
-                of blocks allocated. In general, only one of these numbers will match the
-                corresponding input argument, but this is not guaranteed.
+            Tuple[int, torch.Tensor]: The tuple of number of tokens scheduled and number
+                of blocks allocated (per KV cache). In general, only one of these numbers will
+                match the corresponding input argument, but this is not guaranteed.
         """
         raise NotImplementedError()
 
@@ -212,9 +212,10 @@ class DSInferenceModelBase(torch.nn.Module, ABC):
         raise NotImplementedError()
 
     @abstractmethod
-    def kv_cache_config(self) -> KVCacheConfig:
+    def kv_cache_config(self) -> Tuple[KVCacheConfig, ...]:
         """
-        Return the KV-cache configuration for this model.
+        Return the KV-cache configuration for this model. This should be a tuple of one or more
+        KVCacheConfig objects (one for each distinct cache group).
         """
         raise NotImplementedError()
 
@@ -226,7 +227,7 @@ class DSInferenceModelBase(torch.nn.Module, ABC):
         """
         ...
 
-    def maybe_free_kv(self, sequence: DSSequenceDescriptor):
+    def maybe_free_kv(self, sequence: DSSequenceDescriptor) -> None:
         """
         After completing a forward pass, determine whether or not the there are any KV blocks
         that maybe freed since they are no longer in use.

deepspeed/inference/v2/model_implementations/inference_transformer_base.py
@@ -324,7 +324,7 @@ class DSTransformerModelBase(DSInferenceModelBase):
         self.attn = heuristics.instantiate_attention(attn_config, self._engine_config)
 
     def get_kv_requirements(self, sequence: DSSequenceDescriptor, max_new_tokens: int,
-                            max_new_blocks: int) -> Tuple[int, int]:
+                            max_new_blocks: int) -> Tuple[int, torch.Tensor]:
         """
         See ``DSInferenceModelBase.get_kv_requirements`` for documentation.
 
@@ -341,7 +341,7 @@ class DSTransformerModelBase(DSInferenceModelBase):
         token_capacity = (max_new_blocks +
                           sequence.cur_allocated_blocks) * self.attn.kv_block_size - sequence.seen_tokens
 
-        return token_capacity, max_new_blocks
+        return token_capacity, torch.tensor([max_new_blocks])
 
     def maybe_allocate_kv(self, sequence: DSSequenceDescriptor, n_new_tokens: int) -> None:
         """
@@ -356,7 +356,7 @@ class DSTransformerModelBase(DSInferenceModelBase):
             new_blocks = self.state_manager.allocate_blocks(n_needed_blocks)
             sequence.extend_kv_cache(new_blocks)
 
-    def kv_cache_config(self) -> KVCacheConfig:
+    def kv_cache_config(self) -> Tuple[KVCacheConfig, ...]:
         """
         See ``DSInferenceModelBase.kv_cache_config`` for documentation.
 
@@ -370,7 +370,7 @@ class DSTransformerModelBase(DSInferenceModelBase):
                                                   cache_shape=cache_shape,
                                                   cache_dtype=self.activation_dtype,
                                                   max_blocks_per_allocation_group=max_blocks)
-        return self._kv_cache_config
+        return (self._kv_cache_config, )
 
     def prepare_batch(self, wrapped_batch: RaggedBatchWrapper) -> None:
         """

commit 040ab3ec691a1ab907abb1e6162e391a625a6124

fixes issues introduced in #4668

deepspeed/inference/v2/ragged/ragged_manager.py
@@ -116,7 +116,7 @@ class DSStateManager:
             return
 
         seq = self._seqs[uid]
-        for i in range(self.n_kv_caches):
+        for i in range(self.n_kv_cache_groups):
             self._kv_cache.free(seq.all_block_ids(cache_group=i), cache_group=i)
 
         self._tracking_allocator.free(seq.tracking_id)

deepspeed/inference/v2/ragged/sequence_descriptor.py
@@ -195,7 +195,7 @@ class DSSequenceDescriptor(BaseSequenceDescriptor):
         """
         return self._kv_cache_ids[cache_group].data_ptr()
 
-    @property
+    #TODO: this was previously a property but causing issues with PR-4668 need to consult w. Connor
     def all_block_ids(self, cache_group: int = 0) -> torch.Tensor:
         """
         Return the Tensor containing all block IDs for this sequence in the specified cache group.