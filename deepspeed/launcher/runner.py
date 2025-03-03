ff214de6d9a807dfd55a1da1342a0433571c849c
Bruno Magalhaes <bruno.magalhaes@synthesia.io>
2025-03-03 11:25:37 +0000

    copy past of all files from PR 7020
    Signed-off-by: Bruno Magalhaes <bruno.magalhaes@synthesia.io>

--- deepspeed/launcher/runner.py
+++ deepspeed/launcher/runner.py
@@ -485,13 +485,13 @@ def main(args=None):
             result = subprocess.check_output(hostname_cmd)
         except subprocess.CalledProcessError as err:
             logger.error(
-                "Unable to detect suitable master address via `hostname -I`, please manually specify one via --master_addr"
+                "Unable to detect suitable master address via 'hostname -I', please manually specify one via --master_addr"
             )
             raise err
         args.master_addr = result.decode('utf-8').split()[0]
         if not args.master_addr:
             raise RuntimeError(
-                f"Unable to detect suitable master address via `hostname -I`, please manually specify one via --master_addr"
+                f"Unable to detect suitable master address via 'hostname -I', please manually specify one via --master_addr"
             )
         logger.info(f"Using IP address of {args.master_addr} for node {first_host}")
 
