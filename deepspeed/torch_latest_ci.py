68a44dd7965b0ff14d44d35b80ea6a89dc0b1620
Olatunji Ruwase <tunji.ruwase@snowflake.com>
2025-05-16 22:11:40 +0000

    Modal CI
    Signed-off-by: Olatunji Ruwase <tunji.ruwase@snowflake.com>

--- deepspeed/torch_latest_ci.py
+++ deepspeed/torch_latest_ci.py
@@ -2,19 +2,21 @@ from pathlib import Path
 
 import modal
 
-ROOT_PATH = Path(__file__).parent
+ROOT_PATH = Path(__file__).parent.parent
 
 image = (modal.Image.from_registry(
     "pytorch/pytorch:2.3.0-cuda12.1-cudnn8-devel",
-    add_python="3.10").run_commands("apt update && apt install -y libaio-dev").pip_install_from_requirements(
-        ROOT_PATH / "requirements/requirements-dev.txt", gpu="any"))
+    add_python="3.10").run_commands("apt update && apt install -y libaio-dev")
+    .pip_install_from_requirements(ROOT_PATH / "requirements/requirements.txt", gpu="any")
+    .pip_install_from_requirements(ROOT_PATH / "requirements/requirements-dev.txt", gpu="any"))
+
 
 app = modal.App("deepspeedai-ci", image=image)
 
 
 @app.function(
     gpu="a10g:2",
-    secrets=[modal.Secret.from_local_environ(["HF_TOKEN"])],
+    # secrets=[modal.Secret.from_local_environ(["HF_TOKEN"])],
     timeout=1500,
 )
 def pytest():
