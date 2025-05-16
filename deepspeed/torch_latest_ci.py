8f63a4c67d24807a7bd269592bf806bb5836e00f
Olatunji Ruwase <tunji.ruwase@snowflake.com>
2025-05-16 22:54:09 +0000

    uv for ds install
    Signed-off-by: Olatunji Ruwase <tunji.ruwase@snowflake.com>

--- deepspeed/torch_latest_ci.py
+++ deepspeed/torch_latest_ci.py
@@ -6,9 +6,12 @@ ROOT_PATH = Path(__file__).parent.parent
 
 image = (modal.Image.from_registry(
     "pytorch/pytorch:2.3.0-cuda12.1-cudnn8-devel",
-    add_python="3.10").run_commands("apt update && apt install -y libaio-dev")
+    add_python="3.10")
+    .run_commands(f"pwd && ls -l {ROOT_PATH}")
+    .run_commands("apt update && apt install -y libaio-dev")
     .pip_install_from_requirements(ROOT_PATH / "requirements/requirements.txt", gpu="any")
-    .pip_install_from_requirements(ROOT_PATH / "requirements/requirements-dev.txt", gpu="any"))
+    .pip_install_from_requirements(ROOT_PATH / "requirements/requirements-dev.txt", gpu="any")
+)
 
 
 app = modal.App("deepspeedai-ci", image=image)
