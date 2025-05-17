5a6d2c841f146e5461b6e5db676d477c451a7988
Olatunji Ruwase <tunji.ruwase@snowflake.com>
2025-05-17 00:33:06 +0000

    Handle folders and symlinks
    Signed-off-by: Olatunji Ruwase <tunji.ruwase@snowflake.com>

--- deepspeed/torch_latest_ci.py
+++ deepspeed/torch_latest_ci.py
@@ -7,10 +7,16 @@ ROOT_PATH = Path(__file__).parent.parent
 image = (modal.Image.from_registry(
     "pytorch/pytorch:2.3.0-cuda12.1-cudnn8-devel",
     add_python="3.10")
-    .run_commands(f"pwd && ls -l {ROOT_PATH}")
     .run_commands("apt update && apt install -y libaio-dev")
     .pip_install_from_requirements(ROOT_PATH / "requirements/requirements.txt", gpu="any")
     .pip_install_from_requirements(ROOT_PATH / "requirements/requirements-dev.txt", gpu="any")
+    .add_local_dir(ROOT_PATH / "accelerator", remote_path="/root/accelerator")
+    .add_local_dir(ROOT_PATH / "accelerator", remote_path="/root/deepspeed/accelerator")
+    .add_local_dir(ROOT_PATH / "csrc", remote_path="/root/csrc")
+    .add_local_dir(ROOT_PATH / "csrc", remote_path="/root/deepspeed/ops/csrc")
+    .add_local_dir(ROOT_PATH / "op_builder", remote_path="/root/op_builder")
+    .add_local_dir(ROOT_PATH / "op_builder", remote_path="/root/deepspeed/ops/op_builder")
+    .add_local_dir(ROOT_PATH / "tests", remote_path="/root/tests")
 )
 
 
