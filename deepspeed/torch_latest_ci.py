8c76099890bc12b019319e0f06c866d00a9191ae
Olatunji Ruwase <tunji.ruwase@snowflake.com>
2025-05-17 18:41:16 +0000

    Full torch latest UT
    Signed-off-by: Olatunji Ruwase <tunji.ruwase@snowflake.com>

--- deepspeed/torch_latest_ci.py
+++ deepspeed/torch_latest_ci.py
@@ -9,21 +9,21 @@ import modal
 
 ROOT_PATH = Path(__file__).parent.parent
 
-image = (modal.Image.from_registry(
-    "pytorch/pytorch:2.6.0-cuda12.4-cudnn9-devel",
-    add_python="3.10")
-    .run_commands("apt update && apt install -y libaio-dev")
-    .run_commands("pip list")
-    .pip_install_from_requirements(ROOT_PATH / "requirements/requirements.txt", gpu="any")
-    .pip_install_from_requirements(ROOT_PATH / "requirements/requirements-dev.txt", gpu="any")
-    .add_local_dir(ROOT_PATH / "accelerator", remote_path="/root/accelerator")
-    .add_local_dir(ROOT_PATH / "accelerator", remote_path="/root/deepspeed/accelerator")
-    .add_local_dir(ROOT_PATH / "csrc", remote_path="/root/csrc")
-    .add_local_dir(ROOT_PATH / "csrc", remote_path="/root/deepspeed/ops/csrc")
-    .add_local_dir(ROOT_PATH / "op_builder", remote_path="/root/op_builder")
-    .add_local_dir(ROOT_PATH / "op_builder", remote_path="/root/deepspeed/ops/op_builder")
-    .add_local_dir(ROOT_PATH / "tests", remote_path="/root/tests")
-)
+# yapf: disable
+image = (modal.Image
+         .from_registry("pytorch/pytorch:2.6.0-cuda12.4-cudnn9-devel", add_python="3.10")
+         .run_commands("apt update && apt install -y libaio-dev")
+         .run_commands("pip list")
+         .pip_install_from_requirements(ROOT_PATH / "requirements/requirements.txt", gpu="any")
+         .pip_install_from_requirements(ROOT_PATH / "requirements/requirements-dev.txt", gpu="any")
+         .add_local_dir(ROOT_PATH / "accelerator", remote_path="/root/accelerator")
+         .add_local_dir(ROOT_PATH / "accelerator", remote_path="/root/deepspeed/accelerator")
+         .add_local_dir(ROOT_PATH / "csrc", remote_path="/root/csrc")
+         .add_local_dir(ROOT_PATH / "csrc", remote_path="/root/deepspeed/ops/csrc")
+         .add_local_dir(ROOT_PATH / "op_builder", remote_path="/root/op_builder")
+         .add_local_dir(ROOT_PATH / "op_builder", remote_path="/root/deepspeed/ops/op_builder")
+         .add_local_dir(ROOT_PATH / "tests", remote_path="/root/tests")
+        )
 
 
 app = modal.App("deepspeedai-ci", image=image)
@@ -37,7 +37,7 @@ app = modal.App("deepspeedai-ci", image=image)
 def pytest():
     import subprocess
     subprocess.run(
-        "pytest -sv tests/unit/runtime/test_data.py --torch_ver=2.6".split(),
+        "pytest --forked tests/unit --torch_ver=2.6 --cuda_ver=12.4".split(),
         check=True,
         cwd=ROOT_PATH / ".",
     )
