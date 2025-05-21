84a902c859841c39e58e0751ab8a90e0b6129c07
yisheng <yi.sheng@intel.com>
2025-05-21 11:19:22 +0800

    fix nontorch failure
    Signed-off-by: yisheng <yi.sheng@intel.com>

--- accelerator/real_accelerator.py
+++ accelerator/real_accelerator.py
@@ -131,17 +131,25 @@ def get_accelerator():
         if accelerator_name is None:
             try:
                 import intel_extension_for_pytorch as ipex
+
                 if ipex._C._has_xpu():
                     accelerator_name = "xpu"
             except ImportError as e:
+                pass
+        if accelerator_name is None:
+            try:
                 import torch
+
+                # torch.xpu supported in upstream pytorch.
+                # Currently we can run on xpu device only using pytorch,
+                # also reserve the old path using ipex when the torch version is old.
                 if hasattr(torch, 'xpu'):
-                    if torch.xpu.is_available():
+                    if torch.xpu.device_count() > 0 and torch.xpu.is_available():
                         accelerator_name = "xpu"
                     else:
                         pass
-                else:
-                    pass
+            except ImportError as e:
+                pass
         if accelerator_name is None:
             try:
                 import torch_npu  # noqa: F401,F811 # type: ignore
