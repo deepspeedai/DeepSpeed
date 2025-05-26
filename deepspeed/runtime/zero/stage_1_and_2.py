ba8ef07894e0464a41bacd4602d84cf00869eccf
Armin Zhu <mingzhengzhu1998@gmail.com>
2025-05-26 13:51:04 +0800

    Update stage_1_and_2.py
    format

--- deepspeed/runtime/zero/stage_1_and_2.py
+++ deepspeed/runtime/zero/stage_1_and_2.py
@@ -1903,8 +1903,7 @@ class DeepSpeedZeroOptimizer(ZeROOptimizer):
                 fp32_partition = self.single_partition_of_fp32_groups[i]
                 bit16_partition_buffer = self.param_buffer_of_bit16_for_cpu_offload_groups[i]
                 bit16_partition_buffer.data.copy_(fp32_partition.data)
-                bit16_partitions[partition_id].data.copy_(
-                    bit16_partition_buffer.data, non_blocking=True)
+                bit16_partitions[partition_id].data.copy_(bit16_partition_buffer.data, non_blocking=True)
 
                 self.timers(OPTIMIZER_STEP_TIMER).stop()
             else:
