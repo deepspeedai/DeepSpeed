Fix bugs in ZenFlow + ZeRO Stage 1 and gradient reduction logic
- Fixed the invocation of `reduce_gradients` in ZenFlow + ZeRO Stage 1
- Corrected the reduction logic in `extra_large_grad_reduc` to handle gradient aggregation properly
- Fixed a bug where ZenFlow could not initialize if the user did not provide a dataset

Signed-off-by: Yusen Wu <xrn4ub@virginia.edu>

deepspeed/ops/adam/zenflow_cpu_adam.py
@@ -13,10 +13,10 @@ class ZenFlowCPUAdam(DeepSpeedCPUAdam):
         super(ZenFlowCPUAdam, self).__init__(*args, **kwargs)
         self.overlap_step = overlap_step
         if not self.overlap_step:
-            print("ZenFlowCPUAdam initialized with overlap step.")
+            print("ZenFlowCPUAdam initialized with normal step.")
             self.step = self._sequential_step
         else:
-            print("ZenFlowCPUAdam initialized with normal step.")
+            print("ZenFlowCPUAdam initialized with overlap step.")
             self.step = self._parallel_step
 
     @torch.no_grad()

deepspeed/runtime/zenflow/zenflow_stage_1_and_2.py
@@ -142,7 +142,10 @@ class ZenFlowZeroOptimizer(DeepSpeedZeroOptimizer):
             self.update_interval = int(zenflow_config.update_interval)
 
         if self.select_strategy == 'epoch':
-            self.select_interval = self.select_interval * zenflow_config.steps_per_epoch
+            if zenflow_config.steps_per_epoch is not None:
+                self.select_interval = self.select_interval * zenflow_config.steps_per_epoch
+            else:
+                self.select_interval = 0
 
         if not self.auto_update and self.select_interval != 0 and self.select_interval < self.update_interval:
             raise ValueError("Select interval must be greater or equal to update interval")
@@ -297,16 +300,16 @@ class ZenFlowZeroOptimizer(DeepSpeedZeroOptimizer):
         rank = dist.get_rank(process_group)
 
         self.grad_buffer = torch.empty(total_size, dtype=self.dtype, device='cuda')
-
+        
+        bucket = self.ipg_buckets[communication_data_type]
         if self.auto_update:
-            self.sum_buffer = torch.empty(len(self.params_in_ipg_bucket) + dist.get_world_size(group=process_group),
+            self.sum_buffer = torch.empty(len(bucket.params) + dist.get_world_size(group=process_group),
                                           dtype=torch.bfloat16,
                                           device='cuda')
 
         group_to_paramlist = {}
 
         # count = 0
-        bucket = self.ipg_buckets[communication_data_type]
         for i, param_idx_in_group, param_id in bucket.params:
             param = self.bit16_groups[i][param_idx_in_group]
 