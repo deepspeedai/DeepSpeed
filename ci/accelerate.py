204259869835337d808a3f85c1473d22764b9080
Olatunji Ruwase <tunji.ruwase@snowflake.com>
2025-08-08 18:08:00 +0000

    Debugging
    Signed-off-by: Olatunji Ruwase <tunji.ruwase@snowflake.com>

--- ci/accelerate.py
+++ ci/accelerate.py
@@ -23,7 +23,7 @@ image = (modal.Image
          .run_commands("uv pip list")
          .pip_install_from_requirements(ROOT_PATH / "requirements/requirements.txt", gpu="any")
          .pip_install_from_requirements(ROOT_PATH / "requirements/requirements-dev.txt", gpu="any")
-         .run_commands("pip show deepspeed")
+        #  .run_commands("pip show deepspeed")
          .add_local_dir(ROOT_PATH / "deepspeed", remote_path="/root/deepspeed")
          .add_local_dir(ROOT_PATH / "accelerator", remote_path="/root/accelerator")
          .add_local_dir(ROOT_PATH / "accelerator", remote_path="/root/deepspeed/accelerator")
