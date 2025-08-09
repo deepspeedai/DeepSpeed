91a4da097d874bcf8cab496e496dcff9d4a4d6ab
Olatunji Ruwase <tunji.ruwase@snowflake.com>
2025-08-09 12:29:22 +0000

    Debug skip
    Signed-off-by: Olatunji Ruwase <tunji.ruwase@snowflake.com>

--- ci/torch_latest.py
+++ ci/torch_latest.py
@@ -15,7 +15,7 @@ image = (modal.Image
          .run_commands("apt update && apt install -y libaio-dev")
          .pip_install_from_requirements(ROOT_PATH / "requirements/requirements.txt", gpu="any")
          .pip_install_from_requirements(ROOT_PATH / "requirements/requirements-dev.txt", gpu="any")
-        .add_local_dir(ROOT_PATH , remote_path="/root/")
+         .add_local_dir(ROOT_PATH , remote_path="/root/")
          .add_local_dir(ROOT_PATH / "accelerator", remote_path="/root/deepspeed/accelerator")
          .add_local_dir(ROOT_PATH / "csrc", remote_path="/root/deepspeed/ops/csrc")
          .add_local_dir(ROOT_PATH / "op_builder", remote_path="/root/deepspeed/ops/op_builder")
