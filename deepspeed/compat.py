fa50c7cf54302005d0cd19afe03e2c6a61c06092
Santi Villalba <sdvillal@gmail.com>
2026-02-05 11:45:48 +0100

    Support new python 3.14 annotation handling
    closes #7673
    
    Signed-off-by: Santi Villalba <sdvillal@gmail.com>

---
+++ deepspeed/compat.py
@@ -0,0 +1,36 @@
+"""Compatibility functions to support wider version ranges for python and dependencies."""
+
+# Copyright (c) Microsoft Corporation.
+# SPDX-License-Identifier: Apache-2.0
+
+# DeepSpeed Team
+
+from typing import Mapping, Any
+try:
+    import annotationlib  # python >= 3.14
+except ImportError:
+    annotationlib = None
+
+# Deal with annotations in python versions >=3.14. See:
+#   - Python 3.4 release notes: https://docs.python.org/3/whatsnew/3.14.html
+#     Porting annotations: https://docs.python.org/3/whatsnew/3.14.html#whatsnew314-porting-annotations
+#   - PEP649: https://peps.python.org/pep-0649/
+#   - PEP749: https://peps.python.org/pep-0749/
+# Backwards compatible, applies best practices (use annotationlib) from python 3.14 onwards.
+
+
+def get_annotations_from_namespace(namespace: Mapping[str, object]) -> dict[str, str]:
+    if annotationlib:
+        annotate_func = annotationlib.get_annotate_from_class_namespace(namespace)
+        if annotate_func is not None:
+            return annotationlib.call_annotate_function(annotate_func, annotationlib.Format.VALUE)
+    return namespace.get("__annotations__", {})
+
+
+def get_annotations(obj: Any) -> dict[str, str]:
+    if annotationlib:
+        return annotationlib.get_annotations(obj)
+    try:
+        return obj.__annotations__
+    except AttributeError:
+        return {}
