fix early termination of gradients issue when using autosp

deepspeed/compile/init_sp.py
@@ -7,8 +7,8 @@ import torch
 from torch.fx import GraphModule
 from .passes.sp_compile import apply_autosp
 
-def init_autosp(sp_size=2, dp_size=1):
+def init_autosp(compile_config):
     def backend_fn(gm: GraphModule, real_inputs):
-        apply_autosp(gm, real_inputs, debug=False, sp_size=sp_size, dp_size=dp_size)    
+        apply_autosp(gm, real_inputs, debug=False, sp_size=compile_config.sp_size, dp_size=compile_config.dp_size)    
         return torch._inductor.compile(gm, real_inputs)
     return backend_fn