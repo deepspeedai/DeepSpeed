b1e16c43ec46366281af2d01f4ac79c4e164992b
Junjie Mao <banxing.mjj@alibaba.inc>
2026-03-04 17:48:11 +0800

    tests: Test ZeRO + Ulysses SP training using ParallelState
    Signed-off-by: Yuqing Li <lyq491672@alibaba-inc.com>
    Signed-off-by: Junjie Mao <junjie.mao@linux.alibaba.com>

--- tests/unit/utils/test_parallel_state.py
+++ tests/unit/utils/test_parallel_state.py
@@ -15,8 +15,9 @@ import pytest
 import torch
 import deepspeed
 import deepspeed.comm as dist
-from unit.common import DistributedTest
+from unit.common import DistributedTest, preferred_dtype
 from unit.simple_model import SimpleModel, random_dataloader
+from unit.util import torch_assert_close
 
 DTYPE = torch.float
 
@@ -315,3 +316,158 @@ class TestAllToAllGroupsWithMPU(DistributedTest):
             assert loss is not None
             engine.backward(loss)
             engine.step()
+
+
+@pytest.mark.parametrize("zero_stage", [0, 1, 2, 3])
+@pytest.mark.parametrize("sp_size", [2])
+class TestUlyssesSPWithParallelState(DistributedTest):
+    world_size = 4
+
+    def test_ulysses_sp_parallel_state(self, zero_stage, sp_size):
+        """Compare loss using mpu=ParallelState to test parallel_state's MPU interface."""
+        from transformers import AutoConfig, AutoModelForCausalLM
+        from transformers.modeling_utils import ALL_ATTENTION_FUNCTIONS
+        from deepspeed.utils.parallel_state_wrappers import get_parallel_state_instance
+        from deepspeed.runtime.sequence_parallel.ulysses_sp import UlyssesSPAttentionHF, UlyssesSPDataLoaderAdapter
+        from deepspeed.runtime.utils import move_to_device
+        from deepspeed.accelerator import get_accelerator
+
+        model_name_or_path = 'hf-internal-testing/tiny-random-LlamaForCausalLM'
+        sequence_parallel_size = sp_size
+        micro_batch_size = 1
+        num_iterations = 10
+
+        seed = 42
+        torch.manual_seed(seed)
+        get_accelerator().manual_seed_all(seed)
+
+        base_config = {
+            "train_micro_batch_size_per_gpu": 1,
+            "zero_optimization": {
+                "stage": zero_stage
+            },
+            "optimizer": {
+                "type": "Adam",
+                "params": {
+                    "lr": 1e-3
+                }
+            }
+        }
+        dtype = preferred_dtype()
+        if dtype == torch.bfloat16:
+            base_config["bf16"] = {"enabled": True}
+        elif dtype == torch.float16:
+            base_config["fp16"] = {"enabled": True, "loss_scale": 1.0}
+
+        def collate_fn(batch):
+            input_ids, position_ids = batch[0]
+            return dict(input_ids=input_ids.unsqueeze(0),
+                        position_ids=position_ids.unsqueeze(0),
+                        labels=input_ids.unsqueeze(0))
+
+        input_ids = torch.randint(1, 100, (num_iterations * 2, 6))
+        position_ids = torch.arange(6).unsqueeze(0).expand(num_iterations * 2, -1)
+        ds = torch.utils.data.TensorDataset(input_ids, position_ids)
+        dl = torch.utils.data.DataLoader(ds, batch_size=micro_batch_size, collate_fn=collate_fn, shuffle=False)
+        batches_full = list(dl)
+
+        hf_model_config = AutoConfig.from_pretrained(model_name_or_path)
+        core_attn_implementation = "sdpa"
+        core_attn_function = ALL_ATTENTION_FUNCTIONS[core_attn_implementation]
+
+        # No-SP model
+
+        model_no_sp = AutoModelForCausalLM.from_pretrained(model_name_or_path)
+        model_no_sp, _, _, _ = deepspeed.initialize(config=base_config,
+                                                    model=model_no_sp,
+                                                    model_parameters=model_no_sp.parameters(),
+                                                    mpu=None)
+
+        losses_no_sp = []
+        for i in range(num_iterations):
+            batch = move_to_device(batches_full[i], model_no_sp.device)
+            loss = model_no_sp(**batch).loss
+            model_no_sp.backward(loss)
+            model_no_sp.step()
+            losses_no_sp.append(loss.detach().cpu())
+
+        # SP model
+        #
+        # UlyssesSPAttentionHF.register_with_transformers() creates and returns an SP-specific parallel state object.
+        # Register explicitly here before UlyssesSPAttentionHF is adapted to the generic ParallelState.
+
+        instance_name = "sp_test_psm"
+        ps = get_parallel_state_instance(instance_name)
+        ps.initialize_model_parallel(sequence_parallel_size=sequence_parallel_size, order="sp-dp")
+        # Get SP group info from the initialized instance
+        sp_group = ps.get_sequence_parallel_group()
+        sp_world_size = ps.get_sequence_parallel_world_size()
+        sp_rank = ps.get_sequence_parallel_rank()
+
+        uattn_sp = UlyssesSPAttentionHF(attn=core_attn_function,
+                                        batch_size=micro_batch_size,
+                                        attn_head_count=hf_model_config.num_attention_heads,
+                                        attn_head_size=getattr(
+                                            hf_model_config, "head_dim",
+                                            hf_model_config.hidden_size // hf_model_config.num_attention_heads),
+                                        kv_head_count=hf_model_config.num_key_value_heads,
+                                        num_hidden_layers=hf_model_config.num_hidden_layers,
+                                        process_group=sp_group,
+                                        seq_length_is_variable=True,
+                                        local_seq_length=None,
+                                        global_seq_length=None)
+
+        def uattn_sp_wrapper(module, query, key, value, attention_mask, *args, **kwargs):
+            return uattn_sp(module, query, key, value, None, *args, **kwargs)
+
+        for key in list(ALL_ATTENTION_FUNCTIONS.keys()):
+            ALL_ATTENTION_FUNCTIONS[key] = uattn_sp_wrapper
+
+        config_sp = dict(base_config)
+        config_sp["sequence_parallel_size"] = sequence_parallel_size
+        config_sp["gradient_accumulation_steps"] = 2
+
+        model_sp = AutoModelForCausalLM.from_pretrained(model_name_or_path)
+
+        # Pass ps instance as mpu
+        model_sp, _, _, _ = deepspeed.initialize(config=config_sp,
+                                                 model=model_sp,
+                                                 model_parameters=model_sp.parameters(),
+                                                 mpu=ps)
+
+        ds_sp = torch.utils.data.TensorDataset(input_ids, position_ids)
+        dl_sp = torch.utils.data.DataLoader(ds_sp, batch_size=micro_batch_size, collate_fn=collate_fn, shuffle=False)
+        dl_sp_sharded = UlyssesSPDataLoaderAdapter(dl_sp,
+                                                   sp_rank=sp_rank,
+                                                   sp_group=sp_group,
+                                                   sp_world_size=sp_world_size,
+                                                   device=model_sp.device)
+
+        losses_sp = []
+        loss_accum = []
+
+        for i, batch_sp in enumerate(dl_sp_sharded):
+            if i >= num_iterations * 2:
+                break
+            batch_sp = move_to_device(batch_sp, model_sp.device)
+            outputs_sp = model_sp(**batch_sp)
+            shift_labels = batch_sp["shift_labels"]
+            loss_sp_local = model_sp.module.loss_function(logits=outputs_sp.logits,
+                                                          labels=None,
+                                                          shift_labels=shift_labels,
+                                                          vocab_size=model_sp.module.config.vocab_size)
+            losses_per_rank = torch.distributed.nn.functional.all_gather(loss_sp_local, group=sp_group)
+            good_tokens = sum((shift_labels != -100).view(-1))
+            good_tokens_per_rank = torch.distributed.nn.functional.all_gather(good_tokens, group=sp_group)
+            total_loss = sum(losses_per_rank[r] * good_tokens_per_rank[r] for r in range(sp_world_size))
+            loss_sp = total_loss / sum(good_tokens_per_rank)
+            model_sp.backward(loss_sp)
+            model_sp.step()
+            loss_accum.append(loss_sp.detach().cpu())
+            if len(loss_accum) == 2:
+                avg_loss = torch.stack(loss_accum).mean()
+                losses_sp.append(avg_loss)
+                loss_accum = []
+
+        for i in range(num_iterations):
+            torch_assert_close(losses_no_sp[i], losses_sp[i], rtol=1.6e-02, atol=1e-03)
