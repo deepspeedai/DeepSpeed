b01922ac9ce1506d0e68ccdda1293961d83a8b15
PKUWZP <zhipeng.rainbowserie@gmail.com>
2026-08-22 17:59:15 -0700

    Drop Microsoft copyright docstring from op_builder/mps/__init__.py
    Signed-off-by: PKUWZP <zhipeng.rainbowserie@gmail.com>

--- op_builder/mps/__init__.py
+++ op_builder/mps/__init__.py
@@ -1,7 +1,6 @@
 # SPDX-License-Identifier: Apache-2.0
 
 # DeepSpeed Team
-'''Copyright The Microsoft DeepSpeed Team'''
 
 from .fused_adam import FusedAdamBuilder
 from .no_impl import NotImplementedBuilder
